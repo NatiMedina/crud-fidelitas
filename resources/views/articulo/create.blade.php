5d4aafadb67c41930524511b9563b0fdb41fcc26
naty <natymedina0589@gmail.com>
2021-05-10 13:48:59 -0300

    add create form

--- resources/views/articulo/create.blade.php
+++ resources/views/articulo/create.blade.php
@@ -2,20 +2,21 @@
 
 @section('contenido')
 <form action="/articulos" method="POST">
-    <h1 class="pt-5 pb-5 text-center">Agregar Artículo</h1>
 @csrf
+
+    <h1 class="pt-5 pb-5 text-center">Agregar Artículo</h1>
     <div class="form-group">
         <label for="nombre">Nombre</label>
-        <input type="text" class="form-control" id="nombre"
+        <input type="text" class="form-control" id="nombre" name='nombre'
             placeholder="Nombre del articulo">
     </div>
     <div class="form-group">
         <label for="descripcion">Descripcion</label>
-        <textarea class="form-control" id="descripcion" rows="3"></textarea>
+        <textarea class="form-control" id="descripcion" rows="3" name='descripcion'></textarea>
     </div>
     <div class="form-group">
         <label for="cantidad">Cantidad</label>
-        <input type="number" class="form-control" id="cantidad" min="0" max="1000">
+        <input type="number" class="form-control" id="cantidad" min="0" max="1000" name='cantidad'>
     </div>
     <a href="/articulos" class="btn btn-secondary"> Cancelar </a>
     <button type="submit" class="btn btn-primary"> Guardar</button>
