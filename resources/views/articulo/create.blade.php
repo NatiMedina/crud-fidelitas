7c859e2e824285cbbf36bb0e52fab9b1326e3631
naty <natymedina0589@gmail.com>
2021-05-10 13:31:54 -0300

    add create form

--- resources/views/articulo/create.blade.php
+++ resources/views/articulo/create.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
-@section('content')
+@section('contenido')
 
+<form action="/articulos" method="POST">
+@csrf
+    <div class="form-group">
+        <label for="nombre">Nombre</label>
+        <input type="text" class="form-control" id="nombre"
+            placeholder="Nombre del articulo">
+    </div>
+    <div class="form-group">
+        <label for="descripcion">Descripcion</label>
+        <textarea class="form-control" id="descripcion" rows="3"></textarea>
+    </div>
+    <div class="form-group">
+        <label for="cantidad">Cantidad</label>
+        <input type="number" class="form-control" id="cantidad" min="0" max="1000">
+    </div>
+    <a href="/articulos" class="btn btn-secondary"> Cancelar </a>
+    <button type="submit" class="btn btn-primary"> Guardar</button>
+</form>
 @endsection
